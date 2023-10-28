9abc2e1a1b548c27824de1f583af5654606560ac
Vlastimil Holer <vlastimil.holer@gmail.com>
2023-10-28 20:43:31 +0200

    Set system.logging

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -847,3 +847,6 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
 );
+
+# Please let's this CRAP log something
+$config['system.logging']['error_level'] = 'verbose';
